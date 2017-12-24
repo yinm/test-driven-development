be9844eacd0208a8a20a3258488d5c7415210df5
yinm <yinm.cminor9@gmail.com>
2017-12-24 12:33:30 +0900

    テストを通すため、abstractクラスにして、timesメソッドを定義する

--- src/money/Money.java
+++ src/money/Money.java
@@ -1,7 +1,9 @@
 package money;
 
-class Money {
+abstract class Money {
 	protected int amount;
+	
+	abstract Money times(int multiplier);
 	
 	public boolean equals(Object object) {
 		Money money = (Money) object;
