ea269450799248b49d4f44d189467d606b7d4674
yinm <yinm.cminor9@gmail.com>
2017-12-25 08:22:52 +0900

    インターフェースに引き上げたので、キャストやクラスのチェックは不要になるため削除する

--- src/money/Bank.java
+++ src/money/Bank.java
@@ -2,9 +2,6 @@ package money;
 
 class Bank {
 	Money reduce(Expression source, String to) {
-		if (source instanceof Money)
-			return ((Money) source).reduce(to);
-		Sum sum = (Sum) source;
-		return sum.reduce(to);
+		return source.reduce(to);
 	}
 }
