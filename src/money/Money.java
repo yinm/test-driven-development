018ff129a747dc639e1b8dd0d94bb9acd192e63e
yinm <yinm.cminor9@gmail.com>
2017-12-24 22:17:25 +0900

    インスタンンス化できるように、具象クラスに変更する

--- src/money/Money.java
+++ src/money/Money.java
@@ -1,6 +1,6 @@
 package money;
 
-abstract class Money {
+class Money {
 	protected int amount;
 	protected String currency;
 	
@@ -9,7 +9,9 @@ abstract class Money {
 		this.currency = currency;
 	}
 	
-	abstract Money times(int multiplier);
+	Money times(int multiplier) {
+		return null;
+	}
 	
 	String currency() {
 		return currency;
