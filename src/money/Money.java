fa588429fdf61e1918301df17284c692be4ad97f
yinm <yinm.cminor9@gmail.com>
2017-12-24 23:01:41 +0900

    テストが通るように、Expressionのインターフェースを使う

--- src/money/Money.java
+++ src/money/Money.java
@@ -1,6 +1,6 @@
 package money;
 
-class Money {
+class Money implements Expression {
 	protected int amount;
 	protected String currency;
 	
@@ -13,7 +13,7 @@ class Money {
 		return new Money(amount * multiplier, currency);
 	}
 	
-	Money plus(Money addend) {
+	Expression plus(Money addend) {
 		return new Money(amount + addend.amount, currency);
 	}
 	
