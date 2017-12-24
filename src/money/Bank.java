d7ae85eaa636bcf84009dc7b5cbfeeb06b2249b5
yinm <yinm.cminor9@gmail.com>
2017-12-25 08:18:19 +0900

    Moneyインスタンスを渡す場合の実装を追加する

--- src/money/Bank.java
+++ src/money/Bank.java
@@ -2,6 +2,8 @@ package money;
 
 class Bank {
 	Money reduce(Expression source, String to) {
+		if (source instanceof Money)
+			return (Money) source;
 		Sum sum = (Sum) source;
 		return sum.reduce(to);
 	}
