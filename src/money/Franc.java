e4ed138effd8a3425f5289a73365ec1ac88b3f06
yinm <yinm.cminor9@gmail.com>
2017-12-24 12:09:39 +0900

    親クラスと同じ内容にできたので、削除する

--- src/money/Franc.java
+++ src/money/Franc.java
@@ -8,9 +8,4 @@ class Franc extends Money {
 	Franc times(int multiplier) {
 		return new Franc(amount * multiplier);
 	}
-	
-	public boolean equals(Object object) {
-		Money money = (Money) object;
-		return amount == money.amount;
-	}
 }
