746cdf7e7e910611ab940a87d82c6c00e3bc085b
yinm <yinm.cminor9@gmail.com>
2017-12-25 08:15:35 +0900

    リファクタリングする

--- src/money/Bank.java
+++ src/money/Bank.java
@@ -3,7 +3,6 @@ package money;
 class Bank {
 	Money reduce(Expression source, String to) {
 		Sum sum = (Sum) source;
-		int amount = sum.augend.amount + sum.addend.amount;
-		return new Money(amount, to);
+		return sum.reduce(to);
 	}
 }
--- src/money/Sum.java
+++ src/money/Sum.java
@@ -3,8 +3,14 @@ package money;
 class Sum implements Expression {
 	Money augend;
 	Money addend;
+	
 	Sum(Money augend, Money addend) {
 		this.augend = augend;
 		this.addend = addend;
+	}
+	
+	Money reduce(String to) {
+		int amount = augend.amount + addend.amount;
+		return new Money(amount, to);
 	}
 }
