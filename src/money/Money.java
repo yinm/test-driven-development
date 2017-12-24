dc3fea732383873cbc55f214ea3ca237d187cd1c
yinm <yinm.cminor9@gmail.com>
2017-12-24 12:56:50 +0900

    サブクラスの実装を確かめるために、abstractメソッドを定義する

--- src/money/Money.java
+++ src/money/Money.java
@@ -5,6 +5,8 @@ abstract class Money {
 	
 	abstract Money times(int multiplier);
 	
+	abstract String currency();
+	
 	public boolean equals(Object object) {
 		Money money = (Money) object;
 		return amount == money.amount
