aa583a09141fd840f7378a6a434300b178ca816d
yinm <yinm.cminor9@gmail.com>
2017-12-24 12:35:20 +0900

    Moneyクラスにしてテストが通ることを確認する

--- src/money/Money.java
+++ src/money/Money.java
@@ -11,7 +11,7 @@ abstract class Money {
 			&& getClass().equals(money.getClass());
 	}
 	
-	static Dollar dollar(int amount) {
+	static Money dollar(int amount) {
 		return new Dollar(amount);
 	}
 }
