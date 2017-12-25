42175ce03861d306bf3a5b7c3241d75e529fc4c2
yinm <yinm.cminor9@gmail.com>
2017-12-25 20:52:18 +0900

    Bankクラスが為替レートを管理できるように、引数を定義する

--- src/money/Bank.java
+++ src/money/Bank.java
@@ -2,7 +2,7 @@ package money;
 
 class Bank {
 	Money reduce(Expression source, String to) {
-		return source.reduce(to);
+		return source.reduce(this, to);
 	}
 	void addRate(String from, String to, int rate) {
 	}
