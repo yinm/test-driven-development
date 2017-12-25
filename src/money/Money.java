ac49138b2e5a1fcbbf319aa70b407a76d3de4720
yinm <yinm.cminor9@gmail.com>
2017-12-25 22:11:40 +0900

    plusなどの型もExpressionに変更する

--- src/money/Money.java
+++ src/money/Money.java
@@ -9,11 +9,11 @@ class Money implements Expression {
 		this.currency = currency;
 	}
 	
-	Money times(int multiplier) {
+	Expression times(int multiplier) {
 		return new Money(amount * multiplier, currency);
 	}
 	
-	Expression plus(Money addend) {
+	Expression plus(Expression addend) {
 		return new Sum(this, addend);
 	}
 	
