9cdcda1604a90fdde45ea6e61f4b2ddc5eb114dd
yinm <yinm.cminor9@gmail.com>
2017-12-25 21:10:10 +0900

    為替レートを格納・取得できるようにする

--- src/money/Bank.java
+++ src/money/Bank.java
@@ -1,14 +1,20 @@
 package money;
 
+import java.util.Map;
+import java.util.HashMap;
+
 class Bank {
+	private Map<Pair, Integer> rates = new HashMap<>();
+	
 	Money reduce(Expression source, String to) {
 		return source.reduce(this, to);
 	}
 	
 	void addRate(String from, String to, int rate) {
+		rates.put(new Pair(from, to), rate);
 	}
 	
 	int rate(String from, String to) {
-		return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
+		return rates.get(new Pair(from, to));
 	}
 }
