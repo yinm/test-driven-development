3cb2f2ed4082e86d24310aa9c4054c09945f795c
yinm <yinm.cminor9@gmail.com>
2017-12-25 20:49:42 +0900

    グリーンにするために、フランを2で割ってドルにする実装を書く

--- src/money/Money.java
+++ src/money/Money.java
@@ -18,7 +18,8 @@ class Money implements Expression {
 	}
 	
 	public Money reduce(String to) {
-		return this;
+		int rate = (currency.equals("CHF") && to.equals("USD")) ? 2 : 1;
+		return new Money(amount / rate, to);
 	}
 	
 	String currency() {
