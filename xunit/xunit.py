e5e3b7127b1c3bb61edfa1a8f1bf6e03776200fb
yinm <yinm.cminor9@gmail.com>
2017-12-29 10:50:26 +0900

    データと操作が近くなるように、run()を親クラスに引き上げる

--- xunit/xunit.py
+++ xunit/xunit.py
@@ -2,14 +2,14 @@ class TestCase:
     def __init__(self, name):
         self.name = name
 
+    def run(self):
+        method = getattr(self, self.name)
+        method()
+
 class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun = None
         super().__init__(name)
-
-    def run(self):
-        method = getattr(self, self.name)
-        method()
 
     def testMethod(self):
         self.wasRun = 1
