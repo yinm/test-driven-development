779754b2b3a3ef24b21a048adf9b5b99809dfd9f
yinm <yinm.cminor9@gmail.com>
2017-12-29 15:51:18 +0900

    testBrokenMethod()を定義する

--- xunit/xunit.py
+++ xunit/xunit.py
@@ -34,6 +34,9 @@ class WasRun(TestCase):
     def testMethod(self):
         self.log = self.log + "testMethod "
 
+    def testBrokenMethod(self):
+        raise Exception
+
     def tearDown(self):
         self.log = self.log + "tearDown "
 
