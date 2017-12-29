1667ad13b664e3705d162bd28b97aae975f53a5f
yinm <yinm.cminor9@gmail.com>
2017-12-29 10:48:56 +0900

    親クラスに引き上げていいものを引き上げる

--- xunit/xunit.py
+++ xunit/xunit.py
@@ -1,10 +1,11 @@
 class TestCase:
-    pass
+    def __init__(self, name):
+        self.name = name
 
 class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun = None
-        self.name = name
+        super().__init__(name)
 
     def run(self):
         method = getattr(self, self.name)
