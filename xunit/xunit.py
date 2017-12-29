46c78eaade84e22c9d5806cf06ee173a698e052f
yinm <yinm.cminor9@gmail.com>
2017-12-29 10:44:50 +0900

    コンストラクタに渡した名前で、テストメソッドを呼べるようにする

--- xunit/xunit.py
+++ xunit/xunit.py
@@ -1,9 +1,11 @@
 class WasRun:
     def __init__(self, name):
         self.wasRun = None
+        self.name = name
 
     def run(self):
-        self.testMethod()
+        method = getattr(self, self.name)
+        method()
 
     def testMethod(self):
         self.wasRun = 1
