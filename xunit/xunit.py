bf36bc6269d23f170b993974ce0a191c8b16bde9
yinm <yinm.cminor9@gmail.com>
2017-12-29 15:01:05 +0900

    テストメソッドの実行をログに追加する

--- xunit/xunit.py
+++ xunit/xunit.py
@@ -13,11 +13,11 @@ class TestCase:
 class WasRun(TestCase):
     def setUp(self):
         self.wasRun = None
-        self.wasSetUp = 1
         self.log = "setUp"
 
     def testMethod(self):
         self.wasRun = 1
+        self.log = self.log + "testMethod"
 
 class TestCaseTest(TestCase):
     def setUp(self):
