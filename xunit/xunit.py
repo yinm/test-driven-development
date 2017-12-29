0378db1009af0386c663c970d515ef0ac0c5c204
yinm <yinm.cminor9@gmail.com>
2017-12-29 14:59:02 +0900

    処理がわかりやすように、setUpの確認は、ログで行うようにする

--- xunit/xunit.py
+++ xunit/xunit.py
@@ -14,6 +14,7 @@ class WasRun(TestCase):
     def setUp(self):
         self.wasRun = None
         self.wasSetUp = 1
+        self.log = "setUp"
 
     def testMethod(self):
         self.wasRun = 1
@@ -28,7 +29,7 @@ class TestCaseTest(TestCase):
     
     def testSetUp(self):
         self.test.run()
-        assert(self.test.wasSetUp)
+        assert("setUp" == self.test.log)
 
 TestCaseTest("testRunning").run()
 TestCaseTest("testSetUp").run()
