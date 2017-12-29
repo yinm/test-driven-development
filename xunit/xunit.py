2e9bebecaf3c0e01e551a444ca015f288cdd0a4a
yinm <yinm.cminor9@gmail.com>
2017-12-29 10:38:00 +0900

    テストメソッドを直接呼ぶのではなく、インターフェース(run())を用意する

--- xunit/xunit.py
+++ xunit/xunit.py
@@ -2,10 +2,13 @@ class WasRun:
     def __init__(self, name):
         self.wasRun = None
 
+    def run(self):
+        self.testMethod()
+
     def testMethod(self):
         self.wasRun = 1
 
 test = WasRun("testMethod")
 print(test.wasRun)
-test.testMethod()
+test.run()
 print(test.wasRun)
