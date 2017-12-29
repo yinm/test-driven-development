9236cce984a8dcca5608ca418f5fc46fc7cba470
yinm <yinm.cminor9@gmail.com>
2017-12-29 15:33:02 +0900

    テスト結果を出力できるようにするためのテストを追加する

--- xunit/xunit.py
+++ xunit/xunit.py
@@ -29,5 +29,11 @@ class TestCaseTest(TestCase):
         test = WasRun("testMethod")
         test.run()
         assert("setUp testMethod tearDown " == test.log)
+    
+    def testResult(self):
+        test = WasRun("testMethod")
+        result = test.run()
+        assert("1 run, 0 failed" == result.summary())
 
 TestCaseTest("testTemplateMethod").run()
+TestCaseTest("testResult").run()
