99b5133c6e4ecd88038af6854c5f7f9cfb3e954c
yinm <yinm.cminor9@gmail.com>
2017-12-29 15:09:26 +0900

    WasRunのインスタンスを使う箇所が1箇所だけになったので、setUpを削除してシンプルにする

--- xunit/xunit.py
+++ xunit/xunit.py
@@ -18,11 +18,9 @@ class WasRun(TestCase):
         self.log = self.log + "testMethod "
 
 class TestCaseTest(TestCase):
-    def setUp(self):
-        self.test = WasRun("testMethod")
-
     def testTemplateMethod(self):
-        self.test.run()
-        assert("setUp testMethod " == self.test.log)
+        test = WasRun("testMethod")
+        test.run()
+        assert("setUp testMethod " == test.log)
 
 TestCaseTest("testTemplateMethod").run()
