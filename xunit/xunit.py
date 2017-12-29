992d1356191d41178d828e0749b4f203a21655b8
yinm <yinm.cminor9@gmail.com>
2017-12-29 14:41:14 +0900

    記述をシンプルにするために、setUpメソッドに、テストの実行確認フラグを移動する

--- xunit/xunit.py
+++ xunit/xunit.py
@@ -11,11 +11,8 @@ class TestCase:
         method()
 
 class WasRun(TestCase):
-    def __init__(self, name):
-        self.wasRun = None
-        super().__init__(name)
-
     def setUp(self):
+        self.wasRun = None
         self.wasSetUp = 1
 
     def testMethod(self):
